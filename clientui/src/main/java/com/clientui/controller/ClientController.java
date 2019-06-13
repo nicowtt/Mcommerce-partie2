25793f9a529e28e9fa0c2be6fee27aa30751cdd0
nicow <nicobod31@gmail.com>
2019-06-13 17:02:04 +0200

    afficher la liste des produits
    client -> microservice-produit
    http://localhost:8080/

---
+++ clientui/src/main/java/com/clientui/controller/ClientController.java
@@ -0,0 +1,31 @@
+package com.clientui.controller;
+
+
+
+
+import com.clientui.beans.ProductBean;
+import com.clientui.proxies.MicroserviceProduitsProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class ClientController {
+
+    @Autowired
+    private MicroserviceProduitsProxy produitsProxy;
+
+    @RequestMapping("/")
+    public String accueil(Model model){
+
+        List<ProductBean> produits =  produitsProxy.listeDesProduits();
+
+        model.addAttribute("produits", produits);
+
+        return "Accueil";
+    }
+
+}
