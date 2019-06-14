1ee1d051431871f9b19268d4541464a11487d392
nicow <nicobod31@gmail.com>
2019-06-14 12:02:23 +0200

    minor

--- clientui/src/main/java/com/clientui/proxies/MicroserviceCommandeProxy.java
+++ clientui/src/main/java/com/clientui/proxies/MicroserviceCommandeProxy.java
@@ -2,7 +2,6 @@ package com.clientui.proxies;
 
 import com.clientui.beans.CommandeBean;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
